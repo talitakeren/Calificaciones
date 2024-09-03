901714389d3172021196872f1f0e29d14b652531
labf402 <77077633@continental.edu.pe>
2024-09-03 14:41:51 -0500

    CALIFICACION SUBIDA

---
+++ Calificaciones numericas y literarias.py
@@ -0,0 +1,13 @@
+print("Ingrese la Calificacion: ")
+calificacion = float(input())
+
+if(calificacion > 9.0):
+   print("La calificacion es A")
+elif(calificacion> 8.0):
+    print("La calificacion es B")
+elif (calificacion >= 7.5):
+    print("La calificacion es B")
+else:
+    print("La calificacion es R")
+
+#FIN
